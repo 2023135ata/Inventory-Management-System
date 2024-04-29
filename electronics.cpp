cfa529f5b78ebbaa6aa6c092655b0e7329a48976
ataulmusawir <151961947+ataulmusawir@users.noreply.github.com>
2024-04-30 03:01:15 +0500

    Create electronics.cpp

---
+++ electronics.cpp
@@ -0,0 +1,28 @@
+class Electronics
+{
+private:
+	int no_of_electronics;
+	int types_Electronics;
+
+public:
+	
+	void set_no_of_electronics(int a)
+	{
+		no_of_electronics = a;
+	}
+
+	void set_types_Electronics(int b)
+	{
+		types_Electronics = b;
+	}
+
+	int get_no_of_electronics()
+	{
+		return no_of_electronics;
+	}
+
+	int get_types_Electronics()
+	{
+		return types_Electronics;
+	}
+};
