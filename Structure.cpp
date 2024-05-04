3f621b00c8d4447a95563d28eb5e213bb2a480d7
Malayikakhan <168464305+Malayikakhan@users.noreply.github.com>
2024-05-05 00:53:47 +0500

    Create Structure.cpp

---
+++ Structure.cpp
@@ -0,0 +1,17 @@
+// Structure for product
+struct Product
+{
+    int id;
+    string name;
+    float price;
+    int quantity;
+    ProductCategory category;
+};
+
+// Structure for debit card information
+struct DebitCardInfo
+{
+    string cardNumber;
+    string expiryDate;
+    string cvv;
+};
